bfa19ff6bc423ab1ada674046952e53d36a16df7
Matheus Brito <120425592+Matheus-Brito22@users.noreply.github.com>
2023-11-05 09:19:23 -0300

    First commit

---
+++ src/tarefa.ts
@@ -0,0 +1,14 @@
+function multiplicar(a: number, b: number): number {
+    return a * b
+}
+
+const resultado: number = multiplicar(10, 10);
+console.log( resultado);
+
+
+function saudacao(nome:string): string {
+    return "Olá" + nome;
+}
+
+const ola: string = saudacao("Matheus");
+console.log( ola);
